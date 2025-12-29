abbbef704dcf74efcb117f71270b9807e763ab7c
Cole Duffner <cduffner96@Coles-MBP-2.attlocal.net>
2025-12-29 13:45:37 -0500

    feat: new and impoved landing page

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    server: {
+        port: 3000,
+        open: true,
     },
-  },
-  server: {
-    // This tells Vite to listen on all network interfaces, which can help with some host-related issues.
-    host: '0.0.0.0',
-    // You can explicitly set the port here to avoid conflicts with other running applications.
-    port: 3005,
-  },
-  base: './',
 });
